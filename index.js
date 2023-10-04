80491bb3469c415e3175c4c08c712e0867d296d8
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-10-04 08:04:09 +0300

    Assignment completed: Done correctly npm test fail

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue<=400){
+    return "This one is on me!";
+  }
+  else if (someValue>400 && someValue<=2500)
+  {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else{
+    return "No can do.";
+  }
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+  let city;
+  return city==="NYC" ? "Ok, sounds good." : "No go.";
+
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  let tip;
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
 }
+
+scuberGreetingForFeet(199);
